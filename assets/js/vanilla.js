25a3be35ab30658e9701efb9437c7f7abd13cf12
Felipe Travassos <contato@felipetravassos.com>
2022-05-05 21:11:57 -0300

    Particles

--- assets/js/vanilla.js
+++ assets/js/vanilla.js
@@ -1,4 +1,3 @@
-
 var viewport_height = document.documentElement.clientHeight;
 document.getElementById("home").style.height = viewport_height + 'px';
 
@@ -6,43 +5,37 @@ var respbtn = document.querySelectorAll('.respbtn');
 
 
 for (var z = 0; z < respbtn.length; z++) {
-	var elem = respbtn[z];
-	elem.onclick = function () {
-		alert(1);
-		
-	}
-} ;
+    var elem = respbtn[z];
+    elem.onclick = function() {
+        alert(1);
+
+    }
+};
 
 
 function detectscroll() {
 
-	var y = window.scrollY;
-	var calcy = y / 2;
-	document.getElementById("logohome").style.marginBottom = y + 'px';
+    var y = window.scrollY;
+    var calcy = y / 2;
+    document.getElementById("logohome").style.marginBottom = y + 'px';
 
 
-	if (y > 1) {
-		document.getElementById("header").classList.add('topheader');
-	} else {
-		document.getElementById("header").classList.remove('topheader');
-	}
+    if (y > 1) {
+        document.getElementById("header").classList.add('topheader');
+    } else {
+        document.getElementById("header").classList.remove('topheader');
+    }
 }
 
 
 detectscroll();
-window.addEventListener('resize', function () {
-	detectscroll();
-	var viewport_height = document.documentElement.clientHeight;
-	document.getElementById("home").style.height = viewport_height + 'px';
+window.addEventListener('resize', function() {
+    detectscroll();
+    var viewport_height = document.documentElement.clientHeight;
+    document.getElementById("home").style.height = viewport_height + 'px';
 });
 
-window.addEventListener('scroll', function () {
-	detectscroll();
+window.addEventListener('scroll', function() {
+    detectscroll();
 
 });
-
-
-
-
-
-
